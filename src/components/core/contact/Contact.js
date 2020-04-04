5f94a091a06f6777fdb996ac92d673e33bd18b74
imdeepanshugpt <deepanshugupta2501@gmail.com>
2020-04-05 00:01:16 +0530

    resposiveness improved and validation added

--- src/components/core/contact/Contact.js
+++ src/components/core/contact/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import style from './contact.module.css';
 import Grid from '@material-ui/core/Grid';
@@ -8,27 +8,66 @@ import Icon from '@material-ui/core/Icon';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const [name, setName] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [message, setMessage] = useState(false);
+
   function sendEmail(e) {
     e.preventDefault();
-    const template_params = {
-      "sender_name": e.target[0].value,
-      "sender_email": e.target[2].value,
-      "message": e.target[4].value
-    };
-    const service_id = "default_service";
-    const template_id = "email_template_for_personal_website";
-    emailjs.send(service_id, template_id, template_params, 'user_nLWieMD9Vqz3v04LzGtcK')
-      .then((result) => {
-        console.log(result.text);
-        document.getElementById("form").reset();
-      }, (error) => {
-        console.log(error.text);
-      });
+
+    let errorFound = false;
+
+    if (e.target[0].value === "") {
+      setName(true);
+      errorFound = true;
+    } else {
+      setName(false);
+    }
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!(regex.test(e.target[2].value))) {
+      errorFound = true;
+      setEmail(true);
+    } else {
+      setEmail(false);
+    }
+
+    if (e.target[4].value === "") {
+      errorFound = true;
+      setMessage(true);
+    } else {
+      setMessage(false);
+    }
+
+    if (!errorFound) {
+      const template_params = {
+        "sender_name": e.target[0].value,
+        "sender_email": e.target[2].value,
+        "message": e.target[4].value
+      };
+      const service_id = "default_service";
+      const template_id = "email_template_for_personal_website";
+      emailjs.send(service_id, template_id, template_params, 'user_nLWieMD9Vqz3v04LzGtcK')
+        .then((result) => {
+          console.log(result.text);
+          document.getElementById("form").reset();
+        }, (error) => {
+          console.log(error.text);
+        });
+    }
   }
 
+  function setError(type) {
+    if (name && type === "name") {
+      return <p className={style.error}>Please enter a name</p>;
+    } else if (email && type === "email") {
+      return <p className={style.error}>Please enter a valid email</p>;
+    } else if (message && type === "message") {
+      return <p className={style.error}>Please leave a message</p>;
+    }
+  }
   return (
     <div className={style.contact} id="contact">
-      <h1>Message me</h1>
+      <h1 className={style.message}>Message me</h1>
       <form
         id="form"
         onSubmit={(event) => sendEmail(event)}
@@ -46,6 +85,7 @@ const Contact = () => {
               id="fullName"
               label="Full Name"
             />
+            {setError('name')}
           </Grid>
           <Grid item xs={6}>
             <TextField
@@ -57,6 +97,7 @@ const Contact = () => {
               name="email"
               autoComplete="email"
             />
+            {setError('email')}
           </Grid>
           <Grid item xs={12}>
             <TextareaAutosize
@@ -65,13 +106,13 @@ const Contact = () => {
               className={style.area}
               aria-label="text-area"
               rowsMin={10}
-              placeholder="Message" />
+              placeholder="Message *" />
+            {setError('message')}
           </Grid>
         </Grid>
         <Button
           type="submit"
-          variant="contained"
-          color="primary"
+          variant="outlined"
           endIcon={<Icon>send</Icon>}
         >
           Send
--- src/components/core/home/bio/Bio.js
+++ src/components/core/home/bio/Bio.js
@@ -5,12 +5,15 @@ const Bio = () => {
     const profileImage = require('../../../../assets/imdeepanshugpt.jpg');
     return (
         <div className={classes.bio} >
-            <div className="profile-avatar" >
+            <div className={classes.profileAvatar} >
                 <img src={profileImage} alt="ProfileImage" />
             </div>
             <div className={classes.profileDetails} >
                 <h1 className={classes.about}> About me </h1>
-                <p>Hi, I am Deepanshu Gupta, I am computer science engineer, currently working as full stack javascript developer</p>
+                <p>Hi, I am Deepanshu Gupta, I am a computer science engineer who is very dedicated towards his work and passionate about the latest
+                technologies in the market. I always look forward to creating something innovative and that can be used to
+                apply in the real world. I enjoy being challenged, working on projects that require me to work outside my
+                comfort and knowledge set, as continuing to learn new languages, and development techniques are important to me.</p>
             </div>
         </div>
     );
